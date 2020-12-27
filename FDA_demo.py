6305783d5c07b4807234d6e46051a4e5ea214212
Siddharth Shrivastava <sid97.cstaff@klogin01.hpc.iitd.ac.in>
2020-12-27 12:06:14 +0530

    hpc

--- FDA_demo.py
+++ FDA_demo.py
@@ -3,8 +3,8 @@ from PIL import Image
 from utils import FDA_source_to_target_np
 import scipy.misc
 
-im_src = Image.open("demo_images/source.png").convert('RGB')
-im_trg = Image.open("demo_images/target.png").convert('RGB')
+im_src = Image.open("../../scratch/data/cityscapes/leftImg8bit/train/tubingen/tubingen_000015_000019_leftImg8bit.png").convert('RGB')
+im_trg = Image.open("../../scratch/data/dark_zurich/val/rgb_anon/val/night/GOPR0356/GOPR0356_frame_000330_rgb_anon.png").convert('RGB')
 
 im_src = im_src.resize( (1024,512), Image.BICUBIC )
 im_trg = im_trg.resize( (1024,512), Image.BICUBIC )
@@ -18,5 +18,5 @@ im_trg = im_trg.transpose((2, 0, 1))
 src_in_trg = FDA_source_to_target_np( im_src, im_trg, L=0.01 )
 
 src_in_trg = src_in_trg.transpose((1,2,0))
-scipy.misc.toimage(src_in_trg, cmin=0.0, cmax=255.0).save('demo_images/src_in_tar.png')
+scipy.misc.toimage(src_in_trg, cmin=0.0, cmax=255.0).save('demo_images/src1_in_trg3.png')
 
